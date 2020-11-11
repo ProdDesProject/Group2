45160483a7d495b348c82c8c78e64cd0a0d4adb4
DokterPhill <chrisenroel@msn.com>
2020-11-11 14:33:42 +0200

    created bbtest

---
+++ proof_of_concept/BTtest/app/src/main/java/com/oamkgroup2/bttest/MainActivity.java
@@ -0,0 +1,16 @@
+package com.oamkgroup2.bttest;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.util.Log;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("BTtest", "app started");
+    }
+}
