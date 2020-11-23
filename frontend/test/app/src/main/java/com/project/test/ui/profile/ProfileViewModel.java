cb2ba6c71c78337a2a7d009955825d4e05020f36
patryk2710 <patryk2710dd@gmail.com>
2020-11-23 22:57:55 +0200

    created basic test app

---
+++ frontend/test/app/src/main/java/com/project/test/ui/profile/ProfileViewModel.java
@@ -0,0 +1,19 @@
+package com.project.test.ui.profile;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class ProfileViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public ProfileViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is the Profile page");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
---
+++ frontend/test/app/src/main/java/com/project/test/ui/test/TestViewModel.java
@@ -0,0 +1,19 @@
+package com.project.test.ui.test;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class TestViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public TestViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is the test page");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
