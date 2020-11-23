0cec386714345d27933ea215c606c0feabc83e24
DokterPhill <chrisenroel@msn.com>
2020-11-23 12:21:56 +0200

    updated logservice to support new logs

--- backend/heartbeat/src/main/java/com/oamkgroup2/heartbeat/service/LogService.java
+++ backend/heartbeat/src/main/java/com/oamkgroup2/heartbeat/service/LogService.java
@@ -2,11 +2,13 @@ package com.oamkgroup2.heartbeat.service;
 
 import java.util.List;
 
+import com.google.gson.Gson;
 import com.oamkgroup2.heartbeat.model.Log;
 import com.oamkgroup2.heartbeat.repository.LogRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.logging.Logger;
 
 @Service
 public class LogService {
@@ -14,15 +16,21 @@ public class LogService {
     @Autowired
     private LogRepository logRepository;
 
-	public List<Log> getAll() {
-                return logRepository.findAll();
-	}
+    private final Gson GSON = new Gson();
+    private static final Logger LOG = Logger.getLogger(LogService.class.getName());
 
-	public Log newLog(String log) {
-        //TODO: validate
-                Log newLog = new Log();
-                newLog.setContent(log);
-                return logRepository.save(newLog);
-	}
-    
+    public List<Log> getAll() {
+        return logRepository.findAll();
+    }
+
+    public Log newLog(String stringLog) {
+        try {
+            Log newLog = GSON.fromJson(stringLog, Log.class);
+            return logRepository.save(newLog);
+        } catch (Exception e) {
+            LOG.warning(e.getLocalizedMessage());
+            return null;
+        }
+    }
+
 }
