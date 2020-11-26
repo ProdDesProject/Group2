38157510f9df7575ff91cc26f9559e5265389e61
DokterPhill <chrisenroel@msn.com>
2020-11-26 17:07:02 +0200

    added controller sanity tests

--- backend/heartbeat/src/main/java/com/oamkgroup2/heartbeat/repository/LogRepository.java
+++ backend/heartbeat/src/main/java/com/oamkgroup2/heartbeat/repository/LogRepository.java
@@ -1,7 +1,5 @@
 package com.oamkgroup2.heartbeat.repository;
 
-import java.util.Collection;
-
 import com.oamkgroup2.heartbeat.model.Log;
 
 import org.springframework.data.jpa.repository.JpaRepository;
--- backend/heartbeat/src/test/java/com/oamkgroup2/heartbeat/HeartbeatApplicationTests.java
+++ backend/heartbeat/src/test/java/com/oamkgroup2/heartbeat/HeartbeatApplicationTests.java
@@ -1,13 +1,37 @@
 package com.oamkgroup2.heartbeat;
 
+import com.oamkgroup2.heartbeat.controller.LogController;
+import com.oamkgroup2.heartbeat.controller.ResultController;
+import com.oamkgroup2.heartbeat.controller.UserController;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.logging.Logger;
 
 @SpringBootTest
 class HeartbeatApplicationTests {
 
+	private static final Logger LOG = Logger.getLogger(HeartbeatApplicationTests.class.getName());
+
+	@Autowired
+	LogController logController;
+
+	@Autowired
+	ResultController resultController;
+
+	@Autowired
+	UserController userController;
+
 	@Test
 	void contextLoads() {
+		LOG.info("Starting contextLoads...");
+		assertThat(logController).isNotNull();
+		assertThat(resultController).isNotNull();
+		assertThat(userController).isNotNull();
+		LOG.info("contextLoads complete! Controllers working.");
 	}
 
 }
