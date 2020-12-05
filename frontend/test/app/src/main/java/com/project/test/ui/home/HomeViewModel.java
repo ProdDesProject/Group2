37f44fed7c8f23b4a9eafedb17e922c20e08402f
sean-mcgregor <worksmcg@gmail.com>
2020-12-05 18:40:16 +0200

    Rest Test App
    Rest Test App

--- frontend/test/app/src/main/java/com/project/test/ui/home/HomeViewModel.java
+++ frontend/test/app/src/main/java/com/project/test/ui/home/HomeViewModel.java
@@ -3,6 +3,14 @@ package com.project.test.ui.home;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+import android.widget.TextView;
+import java.util.List;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 
 public class HomeViewModel extends ViewModel {
 
@@ -10,10 +18,50 @@ public class HomeViewModel extends ViewModel {
     private MutableLiveData<String> mainText;
 
     public HomeViewModel() {
+
         mText = new MutableLiveData<>();
-        mText.setValue("this is a Home page");
         mainText = new MutableLiveData<>();
-        mainText.setValue("testingiingngngng");
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://jsonplaceholder.typicode.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        JsonPlaceHolderApi jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
+        Call<List<Post>> call = jsonPlaceHolderApi.getPosts();
+
+        call.enqueue(new Callback<List<Post>>() {
+            @Override
+            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+
+                String stringValue = "";
+
+                if (!response.isSuccessful()) {
+                    mText.setValue("Beep boop");
+                    return;
+                }
+                List<Post> posts = response.body();
+                for (Post post : posts) {
+                    String content = "";
+                    content += "ID: " + post.getId() + "\n";
+                    content += "User ID: " + post.getUserId() + "\n";
+                    content += "Title: " + post.getTitle() + "\n";
+                    content += "Text: " + post.getText() + "\n\n";
+                    stringValue = stringValue + content;
+                }
+
+                //mText.setValue("Success");
+            }
+            @Override
+            public void onFailure(Call<List<Post>> call, Throwable t) {
+                //textViewResult.setText(t.getMessage());
+                mText.setValue("Failure");
+            }
+
+        });
+
+        //mText.setValue("this is a Home page");
+        mainText.setValue("Sean");
     }
 
     public LiveData<String> getText() {
